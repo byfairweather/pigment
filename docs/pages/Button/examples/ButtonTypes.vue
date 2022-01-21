bd20aea66ad44fdcf4b3cfc8d3616ceb6b7e907d
Daniel Fairweather <byfairweather@gmail.com>
2022-01-21 15:57:34 -0800

    Added button color styles

--- docs/pages/Button/examples/ButtonTypes.vue
+++ docs/pages/Button/examples/ButtonTypes.vue
@@ -1,5 +1,30 @@
 <template>
-  <p-button @click="example()">Button</p-button>
+  <p-grid class="widths-3-all spacing-xs-all">
+    <p-grid-item>
+      <p-button @click="example()">Button</p-button>
+    </p-grid-item>
+    <p-grid-item>
+      <p-button class="success" @click="example()">Button</p-button>
+    </p-grid-item>
+    <p-grid-item>
+      <p-button class="warning" @click="example()">Button</p-button>
+    </p-grid-item>
+    <p-grid-item>
+      <p-button class="danger" @click="example()">Button</p-button>
+    </p-grid-item>
+    <p-grid-item>
+      <p-button disabled @click="example()">Button</p-button>
+    </p-grid-item>
+    <p-grid-item>
+      <p-button disabled class="success" @click="example()">Button</p-button>
+    </p-grid-item>
+    <p-grid-item>
+      <p-button disabled class="warning" @click="example()">Button</p-button>
+    </p-grid-item>
+    <p-grid-item>
+      <p-button disabled class="danger" @click="example()">Button</p-button>
+    </p-grid-item>
+  </p-grid>
 </template>
 
 <script lang="ts">
--- docs/pages/Spinner/examples/SpinnerSizes.vue
+++ docs/pages/Spinner/examples/SpinnerSizes.vue
@@ -1,13 +1,25 @@
 <template>
-  <p-grid class="widths-4-all">
-    <p-grid-item>
+  <p-grid>
+    <p-grid-item class="width-4-all">
       <p-spinner class="sm" />
     </p-grid-item>
-    <p-grid-item>
+    <p-grid-item class="width-4-all">
       <p-spinner class="md" />
     </p-grid-item>
-    <p-grid-item>
+    <p-grid-item class="width-4-all">
       <p-spinner class="lg" />
+    </p-grid-item>
+    <p-grid-item class="width-3-all">
+      <p-spinner class="md text-primary" />
+    </p-grid-item>
+    <p-grid-item class="width-3-all">
+      <p-spinner class="md text-success" />
+    </p-grid-item>
+    <p-grid-item class="width-3-all">
+      <p-spinner class="md text-warning" />
+    </p-grid-item>
+    <p-grid-item class="width-3-all">
+      <p-spinner class="md text-danger" />
     </p-grid-item>
   </p-grid>
 </template>
