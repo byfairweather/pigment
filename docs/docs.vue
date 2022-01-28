25376a2f8270aadb9b5d58ceb1c4f7b838741567
Daniel Fairweather <byfairweather@gmail.com>
2022-01-28 10:18:57 -0800

    Added Padding and Margin helpers

--- docs/docs.vue
+++ docs/docs.vue
@@ -1,6 +1,25 @@
 <template>
+  <p-panel class="margin-vertical-md">
+    <p-navbar>
+      <template v-slot:logo>
+        <p-navbar-item><h1 class="font-md">Pigment</h1></p-navbar-item>
+      </template>
+      <template v-slot:start>
+        <p-navbar-item>Item 1</p-navbar-item>
+        <p-navbar-item>Item 2</p-navbar-item>
+        <p-navbar-item>Item 3</p-navbar-item>
+      </template>
+      <template v-slot:mobile>
+        <p-navbar-item>Item 1</p-navbar-item>
+        <p-navbar-item>Item 2</p-navbar-item>
+        <p-navbar-item>Item 3</p-navbar-item>
+      </template>
+      <template v-slot:end>
+        <p-navbar-item>Item 4</p-navbar-item>
+      </template>
+    </p-navbar>
+  </p-panel>
   <p-panel>
-    <h1>Pigment Docs</h1>
     <div class="layout">
       <div class="navigation">
         <ul>
