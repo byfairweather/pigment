e899962d4d5b8571c72483227edfd2360aef9aef
Daniel Fairweather <byfairweather@gmail.com>
2022-01-28 17:44:04 -0800

    Fixed Card component shadow issue

--- src/components/PCard/PCard.vue
+++ src/components/PCard/PCard.vue
@@ -1,27 +1,26 @@
 <template>
-  <component
+  <PBox
     class="p-card"
     :class="{ clickable }"
-    :is="clickable ? 'button' : 'div'"
+    :tabindex="clickable ? 0 : undefined"
+    :role="clickable ? 'button' : undefined"
   >
-    <PBox>
-      <div class="image" v-if="$slots.image">
-        <slot name="image"></slot>
-      </div>
-      <div class="content">
-        <span class="title" v-if="$slots.title">
-          <slot name="title"></slot>
-        </span>
-        <span class="subtitle" v-if="$slots.subtitle">
-          <slot name="subtitle"></slot>
-        </span>
-        <span class="body" v-if="$slots.body">
-          <slot name="body"></slot>
-        </span>
-      </div>
-      <slot></slot>
-    </PBox>
-  </component>
+    <div class="image" v-if="$slots.image">
+      <slot name="image"></slot>
+    </div>
+    <div class="content">
+      <span class="title" v-if="$slots.title">
+        <slot name="title"></slot>
+      </span>
+      <span class="subtitle" v-if="$slots.subtitle">
+        <slot name="subtitle"></slot>
+      </span>
+      <span class="body" v-if="$slots.body">
+        <slot name="body"></slot>
+      </span>
+    </div>
+    <slot></slot>
+  </PBox>
 </template>
 
 <script lang="ts">
