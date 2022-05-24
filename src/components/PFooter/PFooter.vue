2bc20d611fba7dfb8871fee040f615a5f13e9ea9
Daniel Fairweather <byfairweather@gmail.com>
2022-05-24 08:55:33 -0700

    Cleaned up PFooter

--- src/components/PFooter/PFooter.vue
+++ src/components/PFooter/PFooter.vue
@@ -8,13 +8,10 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import PGrid from "../PGrid/PGrid.vue";
-import PGridItem from "../PGrid/PGridItem.vue";
 import PPanel from "../PPanel/PPanel.vue";
-import PNavbar from "../PNavbar/PNavbar.vue";
 
 export default defineComponent({
   name: "p-footer",
-  components: { PGrid, PGridItem, PPanel, PNavbar },
+  components: { PPanel },
 });
 </script>
